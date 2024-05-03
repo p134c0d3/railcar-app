468bb984b703d808e8858672ade3ab2608683b04
HarrisAva <140471903+HarrisAva@users.noreply.github.com>
2024-05-02 23:26:10 -0500

    add data table, create order components and create project folders for component, model and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OrderListComponent } from './components/order-list/order-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OrderListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
