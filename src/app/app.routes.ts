92bebcf6fc9a8a5a12d6c8107ea4ba2b07beb17a
HarrisAva <140471903+HarrisAva@users.noreply.github.com>
2024-05-02 23:00:32 -0500

    create project folders and data table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'orders',
+    loadComponent: () => import('./components/order-list/order-list.component').then(c => c.OrderListComponent) },
+
+    { path: 'orders/new',
+    loadComponent: () => import('./components/order-new/order-new.component').then(c => c.OrderNewComponent) },
+
+    { path: 'orders/:id',
+    loadComponent: () => import('./components/order-edit/order-edit.component').then(c => c.OrderEditComponent) },
+
+];
---
+++ src/app/components/order-new/order-new.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-new',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-new.component.html',
+  styleUrl: './order-new.component.scss'
+})
+export class OrderNewComponent {
+
+}
