27628d5e2ca5062553151d5803bacda44fd49f52
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-05-06 20:23:21 -0500

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,11 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'admin', pathMatch: 'full'},
+
+
+
+
+
 
   { path: 'admin', loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent)},
 
